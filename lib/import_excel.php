59f30dd90352c87e548f83d9aca4d965ca21a5b4
fridi <fridi@library.org.il>
2026-06-07 16:43:43 +0300

    חיפוש לקוח ע"פ מזהה בקליטה מספק

--- lib/import_excel.php
+++ lib/import_excel.php
@@ -21,48 +21,62 @@ function import_from_xlsx()
     write_log("_FILES ".json_encode($_FILES));
     if (file_exists ($_FILES["bills"]["tmp_name"])) {
         $tmpFile = $_FILES['bills']['tmp_name'];
+        write_log ('bil  post '.json_encode ($_POST));
         $supplier_id = $_POST["supplier_id"];
-        //$list= get_excel_order_field($supplier_id);//צריך לשמור בטבלה לכל ספק את השדות שלו?
-        $list = array(1=>"date",4=>"client_name",7=>"obligation",10=>"payment_until",13=>"doc_type");
-        $spreadsheet = IOFactory::load($tmpFile);
-        $sheet = $spreadsheet->getActiveSheet();
-        //write_log("sheets ".json_encode($sheet->toArray()));
-        foreach ($sheet->toArray() as $key=>$row_from_file) {
-            if($key==0)continue;
-            $row_to_table =[];
-            $row_to_table["supplier_id"]= $supplier_id;
-            foreach ($list as $index => $field_name) {
-                if (isset($row_from_file[$index])) {
-                    if(empty($row_from_file[$index])&& $field_name =="payment_until"){//אם לא הגיע לחשב לפי תנאי תשלום ללקוח
-
-                    }
-                    elseif ($field_name=="date"|| $field_name =="payment_until"){
-                       // $date = DateTime::createFromFormat('d/m/Y', $row_from_file[$index]);
-                        //$row_to_table[$field_name] = $date->format('Y-m-d');
-
-                    }
-                    else {
-                        $row_to_table[$field_name] = $row_from_file[$index];
+        //4 => "client_name",
+        //$list= get_excel_order_field($supplier_id);//צריך לשמור בטבלה לכל ספק את השדות שלו
+        $list = array(1 => "date", 7 => "obligation", 10 => "payment_until", 13 => "doc_type", 14 => "BnNumber");
+        $spreadsheet = IOFactory::load ($tmpFile);
+        $sheet = $spreadsheet->getActiveSheet ();
+        $BnNumber_index = array_search ($list, "BnNumber");
+        if ($BnNumber_index) {//אם בין השדות בקובץ של הספק יש ח.פ. ללקוח
+            //write_log("sheets ".json_encode($sheet->toArray()));
+            foreach ($sheet->toArray () as $key => $row_from_file) {
+                if ($key == 0) continue;
+                $row_to_table = [];
+                $row_to_table["supplier_id"] = $supplier_id;
+                if (empty($row_from_file[$BnNumber_index])) {//אם לא הגיע ח.פ. ללקוח בקובץ
+                    continue;
+                }
+                $client = get_data_table("clients", array(array("filter_field" => "BnNumber", "filter_value" => $row_from_file[$BnNumber_index])));
+                if(empty($client)){// אם לא נמצא לקוח עם הח.פ. שבשורה זו
+                    continue;
+                }
+                $row_to_table["client_id"] =$client->id;
+                foreach ($list as $index => $field_name) {
+                    if (isset($row_from_file[$index])) {
+                        if (empty($row_from_file[$index]) && $field_name == "payment_until") {//אם לא הגיע לחשב לפי תנאי תשלום ללקוח
+                            $date_index =  array_search ($list,"date");
+                            $date= date('Y-m-d', strtotime($row_from_file[$date_index]));
+                            $row_to_table[$field_name] = get_payment_until($client->payment_term_id,$date);
+                        } elseif ($field_name == "date" || $field_name == "payment_until") {// צריך לשמור קודם את תאריך הקבלה ואח"כ לחשב את תשלום עד
+                            $row_to_table[$field_name] = date('Y-m-d', strtotime($row_from_file[$index]));
+                            // $date = DateTime::createFromFormat('d/m/Y', $row_from_file[$index]);
+                            //$row_to_table[$field_name] = $date->format('Y-m-d');
+                        } else {
+                            $row_to_table[$field_name] = $row_from_file[$index];
+                        }
                     }
                 }
+                write_log ('row_to_table ' . json_encode ($row_to_table));
+                $result = pre_action_query ("collection", $row_to_table);
+                write_log ('pre_action_query ' . json_encode ($result));
+                $ok = 1;// run_action_query ("collection", null, "new", $result);
+                if ($ok) {
+                    $count_success++;
+                }
+                // write_log(json_encode ( $row));
             }
-            write_log ('row_to_table '.json_encode ($row_to_table));
-            $result = pre_action_query ("collection", $row_to_table);
-            write_log ('pre_action_query '.json_encode ($result));
-            $ok = 1;// run_action_query ("collection", null, "new", $result);
-            if($ok){
-                $count_success++;
+            if ($count_success == 0) {
+                $msg = "אירעה שגיאה בעת קליטת הקובץ , הרשומות לא נקלטו";
+            } elseif ($key == $count_success) {
+                $msg = "הקובץ נקלט בהצלחה, {$count_success}   רשומות עודכנו";
+            } elseif ($count_success < $key) {
+                $msg = "הקובץ נקלט בהצלחה, עודכנו {$count_success} רשומות, מתוך {$key} רשומות ";
             }
-           // write_log(json_encode ( $row));
         }
-        if($count_success ==0){
-            $msg="אירעה שגיאה בעת קליטת הקובץ , הרשומות לא נקלטו";
-        }
-        elseif($key==$count_success){
-            $msg="הקובץ נקלט בהצלחה, {$count_success}   רשומות עודכנו";
-        }
-        elseif($count_success<$key){
-            $msg="הקובץ נקלט בהצלחה, עודכנו {$count_success} רשומות, מתוך {$key} רשומות ";
+         else {
+            $msg = "לא נמצא שדה מזהה ללקוחות ";
         }
     }
     else{
